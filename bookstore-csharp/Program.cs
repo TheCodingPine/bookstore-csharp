7126ca746c6578bce995fed6ddfd8704277987e2
Alberto Rosa <alberto.r.93@hotmail.it>
2022-04-04 16:48:15 +0200

    first push | brainstorm

--- bookstore-csharp/Program.cs
+++ bookstore-csharp/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//-Titolo | stringa
+//- Autore | stringa
+//- ISBN | numerico intero
+//- Numero pagine | numerico intero
+//- Peso del libro | numerico float
+//- Larghezza, Altezza e Profondità del libro | numerico float
+//- Valutazione media | numerico float
+//- Numero di recensioni | intero
+//- se è disponibile in versione Kindle | boolean
+//- se la copertina flessibile è disponibile | boolean
+
+
