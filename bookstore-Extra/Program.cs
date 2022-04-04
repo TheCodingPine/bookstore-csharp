9d32e1e7914c21b9a048d5cae5a099f966892db0
Alberto Rosa <alberto.r.93@hotmail.it>
2022-04-04 18:00:37 +0200

    complete readline

--- bookstore-Extra/Program.cs
+++ bookstore-Extra/Program.cs
@@ -1,10 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
-long bookReviewNumber;
-bool versioneKindle;
-bool copertinaFlessibile;
-
 Console.Write("Titolo Libro: ");
 string titolo = Console.ReadLine();
 Console.Write("Autore: ");
@@ -13,12 +8,18 @@ Console.Write("Numero di pagine: ");
 uint pageNumber = uint.Parse(Console.ReadLine()); //posso parsare inline come JS
 Console.Write("L'ISBN è: ");
 long isbn = long.Parse(Console.ReadLine());
-Console.Write("Peso del Libro:");
+Console.Write("Peso del Libro: ");
 float bookWeight = float.Parse(Console.ReadLine());
-Console.Write("Altezza libro:");
+Console.Write("Altezza libro: ");
 double bookHeight = double.Parse(Console.ReadLine());
-Console.Write("Larghezza libro:");
+Console.Write("Larghezza libro: ");
 double bookWidth = double.Parse(Console.ReadLine());
-Console.Write("Spessore libro:");
+Console.Write("Spessore libro: ");
 double bookDepth = double.Parse(Console.ReadLine());
+Console.Write("Numero di Review: ");
+long bookReviewNumber = long.Parse(Console.ReadLine());
+Console.Write("Presente versione Kindle? true/false : ");
+bool versioneKindle = bool.Parse(Console.ReadLine());
+Console.Write("Presente copertina Flessibile? true/false : ");
+bool copertinaFlessibile = bool.Parse(Console.ReadLine());
 
